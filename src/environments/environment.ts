0c01fd4e5621f32ff6fda7b68ac2524a39976f80
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-10-21 21:04:34 -0300

    morango

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyAMPixQiJ6fYooU06NSFOvHlEOd3tEZ2Yg",
+    authDomain: "entregartudo.firebaseapp.com",
+    databaseURL: "https://entregartudo.firebaseio.com",
+    projectId: "entregartudo",
+    storageBucket: "entregartudo.appspot.com",
+    messagingSenderId: "1047674114458",
+    appId: "1:1047674114458:web:4951bf1f8f258902c96a23",
+    measurementId: "G-2PPL3SZPP7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
